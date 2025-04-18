be98123dffbbcb769c5c6c3c1ff9165ddaf330d1
Chris Garrett <chris@c8va.com>
2025-04-18 16:53:13 +0100

    Initial commit

---
+++ spec/flow_state_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FlowState do
+  it "has a version number" do
+    expect(FlowState::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
